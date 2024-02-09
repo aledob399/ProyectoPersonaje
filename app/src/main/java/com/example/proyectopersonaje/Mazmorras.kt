2091a51849b9206040e620e24b728377619ee982
aledob399 <aledob399@alu.centroafuera.es>
2024-02-09 14:13:40 +0100

    a

---
+++ app/src/main/java/com/example/proyectopersonaje/Mazmorras.kt
@@ -0,0 +1,12 @@
+package com.example.proyectopersonaje
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mazmorras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mazmorra)
+
+    }
+}
