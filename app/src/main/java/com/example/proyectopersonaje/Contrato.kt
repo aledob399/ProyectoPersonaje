3b8ce27db6c28e6813a5b98fd4186651b43166a3
aledob399 <aledob399@alu.centroafuera.es>
2024-02-13 12:31:08 +0100

    a

--- app/src/main/java/com/example/proyectopersonaje/Contrato.kt
+++ app/src/main/java/com/example/proyectopersonaje/Contrato.kt
@@ -2,10 +2,15 @@ package com.example.proyectopersonaje
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Contrato : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contrato)
+        val personaje=intent.getParcelableExtra<Personaje>("personaje")
+        val mascotas = intent.getParcelableArrayListExtra<Mascota>("mascotas")
+        val btnContinuar=findViewById<Button>(R.id.continuar)
+        val btnTirar=findViewById<Button>(R.id.Tirar)
     }
 }
--- app/src/main/java/com/example/proyectopersonaje/Gacha.kt
+++ app/src/main/java/com/example/proyectopersonaje/Gacha.kt
@@ -1,50 +1,68 @@
 package com.example.proyectopersonaje
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Gacha : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gacha)
+        val personaje=intent.getParcelableExtra<Personaje>("personaje")
+        val mascotas = intent.getParcelableArrayListExtra<Mascota>("mascotas")
+        val btnContinuar=findViewById<Button>(R.id.continuar)
+        val btnTirar=findViewById<Button>(R.id.Tirar)
+        btnContinuar.setOnClickListener {
+            val intent = Intent(this,Aventura::class.java)
+            intent.putExtra("personaje",personaje)
+            intent.putParcelableArrayListExtra("mascotas", mascotas)
+            startActivity(intent)
+        }
+        btnTirar.setOnClickListener {
+            val num=(1..6).random()
+            val nombres=ArrayList<String>()
+            nombres.add("Juan")
+            nombres.add("Marcos")
+            nombres.add("Julian")
+            nombres.add("Fran")
+            nombres.add("Eisier")
 
-        val num=(1..6).random()
-        val nombres=ArrayList<String>()
-        nombres.add("Juan")
-        nombres.add("Marcos")
-        nombres.add("Julian")
-        nombres.add("Fran")
-        nombres.add("Eisier")
-
-        when(num){
-            1->{
-                var recompensa: Mascota? =null
+            when(num){
+                1->{
+                    var recompensa: Mascota? =null
                     recompensa =  Mascota(nombres[(0..4).random()], Mascota.tipoMascota.entries[(0..4).random()])
-            }
-            2->{
-                var recompensa: Articulo? =null
-                recompensa =  Articulo(Articulo.TipoArticulo.ORO,Articulo.Nombre.MONEDA,0,15,R.drawable.moneda,1)
-            }
-            3->{
-                var recompensa: Articulo? =null
-                recompensa =  Articulo(Articulo.TipoArticulo.ARMA,Articulo.Nombre.ESPADA,2,23,R.drawable.moneda,1)
-            }
-            4->{
-                var recompensa: Articulo? =null
-                recompensa =  Articulo(Articulo.TipoArticulo.ARMA,Articulo.Nombre.DAGA,2,23,R.drawable.moneda,1)
-            }
-            5->{
-                var recompensa: String? ="Nada"
+                }
+                2->{
+                    var recompensa: Articulo? =null
+                    recompensa =  Articulo(Articulo.TipoArticulo.ORO,Articulo.Nombre.MONEDA,0,15,R.drawable.moneda,1)
+                }
+                3->{
+                    var recompensa: Articulo? =null
+                    recompensa =  Articulo(Articulo.TipoArticulo.ARMA,Articulo.Nombre.ESPADA,2,23,R.drawable.moneda,1)
+                }
+                4->{
+                    var recompensa: Articulo? =null
+                    recompensa =  Articulo(Articulo.TipoArticulo.ARMA,Articulo.Nombre.DAGA,2,23,R.drawable.moneda,1)
+                }
+                5->{
+                    var recompensa: String? ="Nada"
+
+                }
+                6->{
+                    var recompensa: Mascota? =null
+                    recompensa =  Mascota(nombres[(0..4).random()], Mascota.tipoMascota.entries[(0..4).random()])
+                }
+
+
 
             }
-            6->{
-                var recompensa: Mascota? =null
-                recompensa =  Mascota(nombres[(0..4).random()], Mascota.tipoMascota.entries[(0..4).random()])
-            }
-
-
 
         }
+
 
     }
 
